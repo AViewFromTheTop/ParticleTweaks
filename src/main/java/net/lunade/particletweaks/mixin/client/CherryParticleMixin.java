34d6c8b411c11de19b82b60f777eecb76fec9295
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-05-12 20:16:45 -0400

    Cherry

---
+++ src/main/java/net/lunade/particletweaks/mixin/client/CherryParticleMixin.java
@@ -0,0 +1,207 @@
+package net.lunade.particletweaks.mixin.client;
+
+import net.lunade.particletweaks.interfaces.ParticleTweakInterface;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.particle.CherryParticle;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.material.FluidState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CherryParticle.class)
+public abstract class CherryParticleMixin extends TextureSheetParticle implements ParticleTweakInterface {
+
+	@Unique
+	private float particleTweaks$scaler = 0.15F;
+	@Unique
+	private float particleTweaks$prevScale = 1F;
+	@Unique
+	private float particleTweaks$scale = 1F;
+	@Unique
+	private float particleTweaks$targetScale = 1F;
+	@Unique
+	private boolean particleTweaks$useNewSystem = false;
+	@Unique
+	private boolean particleTweaks$hasSwitchedToShrinking = false;
+	@Unique
+	private boolean particleTweaks$canShrink = true;
+	@Unique
+	private boolean particleTweaks$fadeInsteadOfShrink = false;
+	@Unique
+	private boolean particleTweaks$switchesExit = false;
+	@Unique
+	private boolean particleTweaks$hasSetMaxLifetime;
+	@Unique
+	private int particleTweaks$maxLifetime;
+	protected CherryParticleMixin(ClientLevel clientLevel, double d, double e, double f) {
+		super(clientLevel, d, e, f);
+	}
+
+	@Inject(method = "<init>*", at = @At("TAIL"))
+	private void particleTweaks$init(CallbackInfo info) {
+		if (CherryParticle.class.cast(this) instanceof ParticleTweakInterface particleTweakInterface) {
+			particleTweakInterface.particleTweaks$setNewSystem(true);
+			particleTweakInterface.particleTweaks$setScaler(0.25F);
+			particleTweakInterface.particleTweaks$setSwitchesExit(true);
+			particleTweakInterface.particleTweaks$setScalesToZero();
+			particleTweakInterface.particleTweaks$setSlowsInWater(true);
+		}
+	}
+
+	@Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+	public void particleTweaks$runScaling(CallbackInfo info) {
+		if (this.particleTweaks$usesNewSystem()) {
+			if (!this.particleTweaks$hasSetMaxLifetime) {
+				this.particleTweaks$hasSetMaxLifetime = true;
+				this.particleTweaks$maxLifetime = this.lifetime;
+			}
+			this.particleTweaks$calcScale();
+			this.lifetime = Math.min(this.lifetime + 1, this.particleTweaks$maxLifetime);
+			if (this.particleTweaks$getScale(0F) < 0.5F && !this.particleTweaks$hasSwitchedToShrinking()) {
+				this.lifetime = Math.min(this.lifetime + 1, this.particleTweaks$maxLifetime);
+			}
+			if (this.particleTweaks$runScaleRemoval()) {
+				this.remove();
+				info.cancel();
+			}
+			if (this.particleTweaks$slowsInWater() && this.level.getFluidState(BlockPos.containing(this.x, this.y, this.z)).is(FluidTags.WATER)) {
+				this.xd *= 0.8;
+				this.yd *= 0.3;
+				this.zd *= 0.8;
+			}
+		}
+	}
+
+	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/CherryParticle;remove()V", ordinal = 1))
+	public void particleTweaks$onRemove(CherryParticle particle) {
+		if (CherryParticle.class.cast(this) instanceof ParticleTweakInterface particleTweakInterface) {
+			if (particleTweakInterface.particleTweaks$usesNewSystem()) {
+				this.lifetime = 0;
+				return;
+			}
+		}
+		this.remove();
+	}
+
+	@Inject(method = "tick", at = @At("TAIL"), cancellable = true)
+	public void particleTweaks$removeOnceSmall(CallbackInfo info) {
+		if (CherryParticle.class.cast(this) instanceof ParticleTweakInterface particleTweakInterface) {
+			if (particleTweakInterface.particleTweaks$usesNewSystem()) {
+				if (particleTweakInterface.particleTweaks$runScaleRemoval()) {
+					CherryParticle.class.cast(this).remove();
+					info.cancel();
+				}
+			}
+		}
+	}
+
+	@Inject(method = "getRenderType", at = @At("HEAD"), cancellable = true)
+	public void particleTweaks$getRenderType(CallbackInfoReturnable<ParticleRenderType> info) {
+		info.setReturnValue(ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT);
+	}
+
+	@Override
+	public float particleTweaks$getScale(float partialTick) {
+		return this.particleTweaks$usesNewSystem() ? Mth.lerp(partialTick, this.particleTweaks$prevScale, this.particleTweaks$scale) : 1F;
+	}
+
+	@Override
+	public void particleTweaks$calcScale() {
+		this.particleTweaks$prevScale = this.particleTweaks$scale;
+		this.particleTweaks$scale += (this.particleTweaks$targetScale - this.particleTweaks$scale) * this.particleTweaks$scaler;
+	}
+
+	@Override
+	public boolean particleTweaks$runScaleRemoval() {
+		if (this.particleTweaks$usesNewSystem()) {
+			this.age = Mth.clamp(age + 1, 0, this.lifetime);
+			if (this.age >= this.lifetime) {
+				this.particleTweaks$hasSwitchedToShrinking = true;
+				if (!this.particleTweaks$canShrink) {
+					return true;
+				}
+				this.particleTweaks$targetScale = 0F;
+				if (this.particleTweaks$prevScale <= 0.005F) {
+					this.particleTweaks$scale = 0F;
+				}
+				return this.particleTweaks$prevScale == 0F;
+			} else {
+				this.particleTweaks$targetScale = 1F;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void particleTweaks$setScaler(float scaler) {
+		this.particleTweaks$scaler = scaler;
+	}
+
+	@Override
+	public void particleTweaks$setNewSystem(boolean set) {
+		this.particleTweaks$useNewSystem = set;
+	}
+
+	@Override
+	public boolean particleTweaks$usesNewSystem() {
+		return this.particleTweaks$useNewSystem;
+	}
+
+	@Override
+	public void particleTweaks$setScalesToZero() {
+		this.particleTweaks$prevScale = 0F;
+		this.particleTweaks$scale = 0F;
+	}
+
+	@Override
+	public boolean particleTweaks$hasSwitchedToShrinking() {
+		return this.particleTweaks$hasSwitchedToShrinking;
+	}
+
+	@Override
+	public void particleTweaks$setCanShrink(boolean canShrink) {
+		this.particleTweaks$canShrink = canShrink;
+	}
+
+	@Override
+	public void particleTweaks$setFadeInsteadOfScale(boolean set) {
+		this.particleTweaks$fadeInsteadOfShrink = set;
+	}
+
+	@Override
+	public boolean particleTweaks$fadeInsteadOfScale() {
+		return this.particleTweaks$fadeInsteadOfShrink;
+	}
+
+	@Override
+	public void particleTweaks$setSwitchesExit(boolean set) {
+		this.particleTweaks$switchesExit = set;
+	}
+
+	@Override
+	public boolean particleTweaks$switchesExit() {
+		return this.particleTweaks$switchesExit;
+	}
+
+	@Unique
+	private boolean particleTweaks$slowsInWater = false;
+
+	@Override
+	public void particleTweaks$setSlowsInWater(boolean set) {
+		this.particleTweaks$slowsInWater = set;
+	}
+	@Override
+	public boolean particleTweaks$slowsInWater() {
+		return this.particleTweaks$slowsInWater;
+	}
+
+}
