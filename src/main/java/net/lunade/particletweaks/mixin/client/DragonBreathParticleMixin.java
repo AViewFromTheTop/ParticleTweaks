f52a06f2dd5a93fba7e8812d5eb201e9ed5ef649
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-08-22 03:20:16 -0400

    add more missing particles

---
+++ src/main/java/net/lunade/particletweaks/mixin/client/DragonBreathParticleMixin.java
@@ -0,0 +1,100 @@
+package net.lunade.particletweaks.mixin.client;
+
+import net.lunade.particletweaks.impl.FluidFallingCalculator;
+import net.lunade.particletweaks.impl.ParticleTweakInterface;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.particle.DragonBreathParticle;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = DragonBreathParticle.class, priority = 1001)
+public abstract class DragonBreathParticleMixin extends TextureSheetParticle {
+
+	protected DragonBreathParticleMixin(ClientLevel clientLevel, double d, double e, double f) {
+		super(clientLevel, d, e, f);
+	}
+
+	@Inject(method = "<init>*", at = @At("TAIL"))
+	private void particleTweaks$init(CallbackInfo info) {
+		if (this instanceof ParticleTweakInterface particleTweakInterface) {
+            particleTweakInterface.particleTweaks$setNewSystem(true);
+            particleTweakInterface.particleTweaks$setScaler(0.2F);
+            particleTweakInterface.particleTweaks$setScalesToZero();
+            particleTweakInterface.particleTweaks$setSwitchesExit(true);
+		}
+	}
+
+	@Inject(method = "tick", at = @At("HEAD"))
+	public void particleTweaks$runScaling(CallbackInfo info) {
+		if (this instanceof ParticleTweakInterface particleTweakInterface) {
+			if (particleTweakInterface.particleTweaks$usesNewSystem()) {
+				particleTweakInterface.particleTweaks$calcScale();
+				this.age = Mth.clamp(age - 1, 0, this.lifetime);
+				if (particleTweakInterface.particleTweaks$getScale(0F) <= 0.85F && !particleTweakInterface.particleTweaks$hasSwitchedToShrinking()) {
+					this.age = Mth.clamp(age - 1, 0, this.lifetime);
+				}
+			}
+			BlockPos blockPos = BlockPos.containing(this.x, this.y, this.z);
+			FluidState fluidState = this.level.getFluidState(blockPos);
+			boolean isFluidHighEnough = false;
+			boolean slowsInWater = particleTweakInterface.particleTweaks$slowsInWater();
+			boolean movesWithWater = particleTweakInterface.particleTweaks$movesWithWater();
+			if (slowsInWater || movesWithWater) {
+				isFluidHighEnough = !fluidState.isEmpty() && (fluidState.getHeight(this.level, blockPos) + (float)blockPos.getY()) >= this.y;
+			}
+
+			if (slowsInWater && isFluidHighEnough) {
+				this.xd *= 0.8;
+				this.yd = FluidFallingCalculator.getFluidFallingAdjustedMovement(this.gravity, this.yd * 0.016D);
+				this.yd += 0.06D;
+				this.zd *= 0.8;
+			}
+			if (movesWithWater && isFluidHighEnough) {
+				Vec3 flow = fluidState.getFlow(this.level, blockPos);
+				this.xd += flow.x() * 0.005D;
+				this.yd += flow.y() * 0.005D;
+				this.zd += flow.z() * 0.005D;
+			}
+		}
+	}
+
+	@Inject(method = "tick", at = @At("TAIL"), cancellable = true)
+	public void particleTweaks$removeOnceSmall(CallbackInfo info) {
+		if (this instanceof ParticleTweakInterface particleTweakInterface) {
+			if (particleTweakInterface.particleTweaks$usesNewSystem()) {
+				if (particleTweakInterface.particleTweaks$runScaleRemoval()) {
+					this.remove();
+					info.cancel();
+				}
+			}
+		}
+	}
+
+	@Inject(method = "getQuadSize", at = @At("RETURN"), cancellable = true)
+	public void particleTweaks$getQuadSize(float partialTicks, CallbackInfoReturnable<Float> info) {
+		if (this instanceof ParticleTweakInterface particleTweakInterface) {
+			if (particleTweakInterface.particleTweaks$usesNewSystem()) {
+				boolean switched = particleTweakInterface.particleTweaks$hasSwitchedToShrinking() && particleTweakInterface.particleTweaks$switchesExit();
+				if (!particleTweakInterface.particleTweaks$fadeInsteadOfScale() && !switched) {
+					info.setReturnValue(info.getReturnValue() * particleTweakInterface.particleTweaks$getScale(partialTicks));
+				} else {
+					this.alpha = particleTweakInterface.particleTweaks$getScale(partialTicks);
+				}
+			}
+		}
+	}
+
+	@Inject(method = "getRenderType", at = @At("HEAD"), cancellable = true)
+	public void particleTweaks$getRenderType(CallbackInfoReturnable<ParticleRenderType> info) {
+		info.setReturnValue(ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT);
+	}
+}
